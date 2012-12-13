29f4b010524940033f3ab40bed069dfe30874c6d
dandello2000 <dandello2000@users.sourceforge.net>
2012-12-13 20:22:02 +0000

    spelling, css and html cleanup, code formatting
    git-svn-id: https://svn.code.sf.net/p/yabb/svn@601 5281d325-37d3-4172-b7de-bd13e9c9f870

--- branches/2.5.4/cgi-bin/yabb2/Admin/ModList.pl
+++ branches/2.5.4/cgi-bin/yabb2/Admin/ModList.pl
@@ -11,145 +11,134 @@
 # Software by:  The YaBB Development Team                                     #
 #               with assistance from the YaBB community.                      #
 ###############################################################################
+use strict;
+use CGI::Carp qw(fatalsToBrowser);
+our $VERSION = 1.1;
 
-$modlistplver = 'YaBB 2.5.4 $Revision: 1.0 $';
-if ($action eq 'detailedversion') { return 1; }
+my $modlistplver = 'YaBB 2.5.4 $Revision: 1.1 $';
+my ($action);
+if ( $action eq 'detailedversion' ) { return 1; }
 
 sub ListMods {
-	my @installed_mods = ();
+    my @installed_mods = ();
 
-	# You need to list your mod in this file for full compliance.
-	# Add it in the following way:
-	# 	$my_mod = "Name of Mod|Author|Description|Version|Date Released";
-	#	push (@installed_mods, "$my_mod");
-	# It is reccomended that you do a "add before" on the end boardmod tag
-	# This preserves the installation order.
+    # You need to list your mod in this file for full compliance.
+    # Add it in the following way:
+    #        $my_mod = "Name of Mod|Author|Description|Version|Date Released";
+    #        push (@installed_mods, "$my_mod");
+    # It is reccomended that you do a "add before" on the end boardmod tag
+    # This preserves the installation order.
 
-	# Also note, you should pick a unique name instead of "$my_mod".
-	# If you mod is called "SuperMod For Doing Cool Things"
-	# You could use "$SuperMod_CoolThings"
+    # Also note, you should pick a unique name instead of "$my_mod".
+    # If your mod is called "SuperMod For Doing Cool Things"
+    # You could use "$SuperMod_CoolThings"
 
 ### BOARDMOD ANCHOR ###
 ### END BOARDMOD ANCHOR ###
+    our ( $yymain, %mod_list, $imagesdir, $yytitle );
+    my ( $action_area,  $mod_text_list, $full_description );
 
-	if (!@installed_mods) {
-
-		$yymain .= qq~
- <div class="bordercolor" style="padding: 0px; width: 99%; margin-left: 0px; margin-right: auto;">
-   <table width="100%" cellspacing="1" cellpadding="4">
-     <tr valign="middle">
-       <td align="left" class="titlebg" colspan="3">
-		 <img src="$imagesdir/preferences.gif" alt="" border="0" /><b>$mod_list{'5'}</b>
-	   </td>
-     </tr>
-     <tr valign="middle">
-       <td align="left" class="windowbg2">
-		  <br />
-		    $mod_list{'8'} <a href="http://www.boardmod.org">$mod_list{'9'}</a>
-		  <br /><br />
-	   </td>
-     </tr>
-   </table>
- </div>
+    if ( !@installed_mods ) {
+        $yymain .= qq~
+<div class="bordercolor rightboxdiv">
+    <table class="cs_1px pad_4px">
+        <tr>
+            <td class="titlebg" colspan="3">
+                <img src="$imagesdir/preferences.gif" alt="" /><b>$mod_list{'5'}</b>
+             </td>
+         </tr><tr>
+             <td class="windowbg2">
+                 <br />
+                 $mod_list{'8'} <a href="http://www.boardmod.org">$mod_list{'9'}</a>
+                 <br /><br />
+             </td>
+         </tr>
+    </table>
+</div>
 ~;
+        $yytitle     = $mod_list{'6'};
+        $action_area = 'modlist';
+        AdminTemplate();
+    }
 
-		$yytitle     = $mod_list{'6'};
-		$action_area = "modlist";
+    foreach my $modification (@installed_mods) {
+        chomp $modification;
+        my ( $mod_anchor, $mod_author, $mod_desc, $mod_version, $mod_date ) =
+          split /\|/xsm, $modification;
 
-		&AdminTemplate;
-	}
+        my $mod_displayname = $mod_anchor;
+        $mod_displayname =~ s/\_/ /gxsm;
+        $mod_anchor      =~ s/ /\_/gsm;
+        $mod_anchor      =~ s/[^\w]//gxsm;
 
-	foreach $modification (@installed_mods) {
-		chomp($modification);
-		($mod_anchor, $mod_author, $mod_desc, $mod_version, $mod_date) = split(/\|/, $modification);
+        $mod_text_list .= qq~<tr>
+            <td class="windowbg2">
+                <a href="#$mod_anchor">$mod_displayname</a>
+            </td>
+            <td class="windowbg2">
+                $mod_author
+            </td>
+            <td class="windowbg2">
+                $mod_version
+            </td>
+        </tr>~;
 
-		$mod_displayname = $mod_anchor;
-		$mod_displayname =~ s/\_/ /g;
-		$mod_anchor      =~ s/ /\_/g;
-		$mod_anchor      =~ s/[^\w]//g;
-
-		$mod_text_list .= qq~
-
-     <tr valign="middle">
-       <td align="left" class="windowbg2">
-		  <a href="#$mod_anchor">$mod_displayname</a>
-	   </td>
-       <td align="left" class="windowbg2">
-		  $mod_author
-	   </td>
-       <td align="left" class="windowbg2">
-		  $mod_version
-	   </td>
-     </tr>
-
-		~;
-
-		$full_description .= qq~
- <div class="bordercolor" style="padding: 0px; width: 99%; margin-left: 0px; margin-right: auto;">
-   <table width="100%" cellspacing="1" cellpadding="4">
-     <tr valign="middle">
-       <td align="left" class="titlebg">
-		 <a name="$mod_anchor"><img src="$imagesdir/preferences.gif" alt="" border="0" /></a><b>$mod_displayname</b> &nbsp; <span class="small">$mod_list{'4'}: $mod_version</span>
-	   </td>
-     </tr>
-     <tr valign="middle">
-       <td align="left" class="catbg">
-		 <span class="small">$mod_list{'2'}: $mod_author</span>
-	   </td>
-     </tr>
-     <tr valign="middle">
-       <td align="left" class="windowbg2"><br />
-		  $mod_desc<br /><br />
-	   </td>
-     </tr>
-       <td align="right" class="catbg">
-		 <span class="small">$mod_list{'3'}: $mod_date</span>
-	   </td>
-     </tr>
-
-   </table>
- </div>
+        $full_description .= qq~
+<div class="bordercolor rightboxdiv">
+    <table class="cs_1px pad_4px">
+        <tr>
+            <td class="titlebg">
+                <a id="$mod_anchor"><img src="$imagesdir/preferences.gif" alt="" /></a><b>$mod_displayname</b> &nbsp; <span class="small">$mod_list{'4'}: $mod_version</span>
+            </td>
+        </tr><tr>
+            <td class="catbg">
+                <span class="small">$mod_list{'2'}: $mod_author</span>
+            </td>
+        </tr><tr>
+            <td class="windowbg2"><br />
+                $mod_desc<br /><br />
+            </td>
+        </tr><tr>
+            <td class="catbg right">
+                <span class="small">$mod_list{'3'}: $mod_date</span>
+            </td>
+        </tr>
+    </table>
+</div>
 <br />
-		~;
+~;
+    }
 
-	}
-
-	$yymain .= qq~
- <div class="bordercolor" style="padding: 0px; width: 99%; margin-left: 0px; margin-right: auto;">
-   <table width="100%" cellspacing="1" cellpadding="4">
-     <tr valign="middle">
-       <td align="left" class="titlebg" colspan="3">
-		 <img src="$imagesdir/preferences.gif" alt="" border="0" /><b>$mod_list{'5'}</b>
-	   </td>
-     </tr>
-     <tr valign="middle">
-       <td align="left" class="catbg">
-		 <span class="small">$mod_list{'1'}</span>
-	   </td>
-       <td align="left" class="catbg">
-		 <span class="small">$mod_list{'2'}</span>
-	   </td>
-       <td align="left" class="catbg">
-		 <span class="small">$mod_list{'4'}</span>
-	   </td>
-     </tr>
-
-$mod_text_list
-     </tr>
-
-   </table>
- </div>
-
+    $yymain .= qq~
+<div class="bordercolor rightboxdiv">
+    <table class="cs_1px pad_4px">
+        <tr>
+            <td class="titlebg" colspan="3">
+                <img src="$imagesdir/preferences.gif" alt="" /><b>$mod_list{'5'}</b>
+            </td>
+        </tr><tr>
+            <td class="catbg">
+                <span class="small">$mod_list{'1'}</span>
+            </td>
+            <td class="catbg">
+                <span class="small">$mod_list{'2'}</span>
+            </td>
+            <td class="catbg">
+                <span class="small">$mod_list{'4'}</span>
+            </td>
+        </tr>
+        $mod_text_list
+        </tr>
+     </table>
+</div>
 <br /><br /><br />
-
 $full_description
-
 ~;
 
-	$yytitle     = $mod_list{'6'};
-	$action_area = "modlist";
-
-	&AdminTemplate;
+    $yytitle     = $mod_list{'6'};
+    $action_area = 'modlist';
+    AdminTemplate();
+    return $yymain;
 }
 
 1;
