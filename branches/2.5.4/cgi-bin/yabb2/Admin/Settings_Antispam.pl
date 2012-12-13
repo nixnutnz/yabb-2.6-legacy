e34a516aa52b131b06d2b69114faf8566b216f92
dandello2000 <dandello2000@users.sourceforge.net>
2012-12-13 20:28:36 +0000

    Perl to strict, css and html cleanup
    git-svn-id: https://svn.code.sf.net/p/yabb/svn@607 5281d325-37d3-4172-b7de-bd13e9c9f870

--- branches/2.5.4/cgi-bin/yabb2/Admin/Settings_Antispam.pl
+++ branches/2.5.4/cgi-bin/yabb2/Admin/Settings_Antispam.pl
@@ -11,158 +11,206 @@
 # Software by:  The YaBB Development Team                                     #
 #               with assistance from the YaBB community.                      #
 ###############################################################################
+use strict;
+use CGI::Carp qw(fatalsToBrowser);
+use English qw(-no_match_vars);
+our $VERSION = 1.1;
 
-$settings_antispamplver = 'YaBB 2.5.4 $Revision: 1.0 $';
-if ($action eq 'detailedversion') { return 1; }
+my ($action);
+my $settings_antispamplver = 'YaBB 2.5.4 $Revision: 1.1 $';
+if ( $action eq 'detailedversion' ) { return 1; }
+
+our (
+    %settings_txt,      %admin_txt,  %tsc_txt,
+    %domain_filter_txt, $spamlist,   $post_speed_count,
+    $minlinkpost,       $minlinksig, $minlinkweb,
+    $spd_detention,     $timeout,    $spd_detention_time,
+    $honeypot,          $spamfruits, $speedpostdetection,
+    $min_post_speed,    $vardir
+);
 
 # TSC
-if (-e "$vardir/spamrules.txt" ) {
-	fopen(SPAM, "$vardir/spamrules.txt") || &fatal_error("cannot_open","spamrules.txt", 1);
-	$spamlist = join('', <SPAM>);
-	fclose(SPAM);
+if ( -e "$vardir/spamrules.txt" ) {
+    no strict;
+    fopen( SPAM, "$vardir/spamrules.txt" )
+      || fatal_error( 'cannot_open', 'spamrules.txt', 1 );
+
+    $spamlist = do { local $INPUT_RECORD_SEPARATOR = undef; <SPAM> };
+    fclose(SPAM);
+    use strict;
 }
 
 # Email Domain Filter
-if (-e "$vardir/email_domain_filter.txt" ) {
-	require "$vardir/email_domain_filter.txt";
+if ( -e "$vardir/email_domain_filter.txt" ) {
+    require "$vardir/email_domain_filter.txt";
 }
-$adomains =~ s~,~\n~g;
-$bdomains =~ s~,~\n~g;
+my $adomains =~ s/,/\n/gsxm;
+my $bdomains =~ s/,/\n/gxsm;
 
 # List of settings
-@settings = (
-{
-	name  => $settings_txt{'generalspam'},
-	id    => 'spam',
-	items => [
-		{
-			description => qq~<label for="post_speed_count">$admin_txt{'91'}<br /><span class="small">$admin_txt{'91a'}</span></label>~,
-			input_html => qq~<input type="text" name="post_speed_count" id="post_speed_count" size="5" value="$post_speed_count" />~,
-			name => 'post_speed_count',
-			validate => 'number',
-		},
-		{
-			description => qq~<label for="minlinkpost">$admin_txt{'minlinkpost'}<br /><span class="small">$admin_txt{'minlinkpost_exp'}</span></label>~,
-			input_html => qq~<input type="text" name="minlinkpost" id="minlinkpost" size="5" value="$minlinkpost" />~,
-			name => 'minlinkpost',
-			validate => 'number',
-		},
-		{
-			description => qq~<label for="minlinksig">$admin_txt{'minlinksig'}<br /><span class="small">$admin_txt{'minlinksig_exp'}</span></label>~,
-			input_html => qq~<input type="text" name="minlinksig" id="minlinksig" size="5" value="$minlinksig" />~,
-			name => 'minlinksig',
-			validate => 'number',
-		},
-		{
-			description => qq~<label for="minlinkweb">$admin_txt{'minlinkweb'}<br /><span class="small">$admin_txt{'minlinkweb_exp'}</span></label>~,
-			input_html => qq~<input type="text" name="minlinkweb" id="minlinkweb" size="5" value="$minlinkweb" />~,
-			name => 'minlinkweb',
-			validate => 'number',
-		},
-		{
-			description => qq~<label for="spd_detention_time">$admin_txt{'92'}<br /><span class="small">$admin_txt{'93'}</span></label>~,
-			input_html => qq~<input type="text" name="spd_detention_time" id="spd_detention_time" size="5" value="$spd_detention_time" />~,
-			name => 'spd_detention_time',
-			validate => 'number',
-		},
-		{
-			description => qq~<label for="timeout">$admin_txt{'408'}</label>~,
-			input_html => qq~<input type="text" name="timeout" id="timeout" size="4" value="$timeout" />~,
-			name => 'timeout',
-			validate => 'number',
-		},
-		{
-			description => qq~<label for="honeypot">$admin_txt{'honeypot'}</label>~,
-			input_html => qq~<input type="checkbox" name="honeypot" id="honeypot" value="1"${ischecked($honeypot)} />~,
-			name => 'honeypot',
-			validate => 'boolean',
-		},
-		{
-			description => qq~<label for="spamfruits">$admin_txt{'spamfruits'}</label>~,
-			input_html => qq~<input type="checkbox" name="spamfruits" id="spamfruits" value="1"${ischecked($spamfruits)} />~,
-			name => 'spamfruits',
-			validate => 'boolean',
-		},
-		{
-			header => $settings_txt{'speedban'},
-		},
-		{
-			description => qq~<label for="speedpostdetection">$admin_txt{'89'}</label>~,
-			input_html => qq~<input type="checkbox" name="speedpostdetection" id="speedpostdetection" value="1" ${ischecked($speedpostdetection)}/>~,
-			name => 'speedpostdetection',
-			validate => 'boolean',
-		},
-		{
-			description => qq~<label for="min_post_speed">$admin_txt{'90'}</label>~,
-			input_html => qq~<input type="text" name="min_post_speed" id="min_post_speed" size="5" value="$min_post_speed" />~,
-			name => 'min_post_speed',
-			validate => 'number',
-			depends_on => ['speedpostdetection'],
-		},
-	],
-},
-{
-	name  => $tsc_txt{'2'},
-	id    => 'tsc',
-	items => [
-		{
-			description => qq~<label for="spamrules"><b>$tsc_txt{'4'}</b><br /><span class="small">$tsc_txt{'3'}</span></label>~,
-			input_html => qq~<textarea cols="60" rows="35" name="spamrules" id="spamrules" style="width: 95%">$spamlist</textarea>~,
-			two_rows => 1,
-			name => 'spamrules',
-			validate => 'fulltext,null',
-		},
-	],
-},
-{
-	name  => $domain_filter_txt{'2'},
-	id    => 'emailfilter',
-	items => [
-		{
-			description => qq~<label for="adomains"><b>$domain_filter_txt{'4'}</b><br /><span class="small">$domain_filter_txt{'3'}</span></label>~,
-			input_html => qq~<textarea cols="60" rows="35" name="adomains" id="adomains" style="width: 95%">$adomains</textarea>~,
-			two_rows => 1,
-			name => 'adomains',
-			validate => 'fulltext,null',
-		},
-		{
-			description => qq~<label for="bdomains"><b>$domain_filter_txt{'6'}</b><br /><span class="small">$domain_filter_txt{'7'}</span></label>~,
-			input_html => qq~<textarea cols="60" rows="35" name="bdomains" id="bdomains" style="width: 95%">$bdomains</textarea>~,
-			two_rows => 1,
-			name => 'bdomains',
-			validate => 'fulltext,null',
-		},
-	],
-},
+our @settings = (
+    {
+        name  => $settings_txt{'generalspam'},
+        id    => 'spam',
+        items => [
+            {
+                description =>
+qq~<label for="post_speed_count">$admin_txt{'91'}<br /><span class="small">$admin_txt{'91a'}</span></label>~,
+                input_html =>
+qq~<input type="text" name="post_speed_count" id="post_speed_count" size="5" value="$post_speed_count" />~,
+                name     => 'post_speed_count',
+                validate => 'number',
+            },
+            {
+                description =>
+qq~<label for="minlinkpost">$admin_txt{'minlinkpost'}<br /><span class="small">$admin_txt{'minlinkpost_exp'}</span></label>~,
+                input_html =>
+qq~<input type="text" name="minlinkpost" id="minlinkpost" size="5" value="$minlinkpost" />~,
+                name     => 'minlinkpost',
+                validate => 'number',
+            },
+            {
+                description =>
+qq~<label for="minlinksig">$admin_txt{'minlinksig'}<br /><span class="small">$admin_txt{'minlinksig_exp'}</span></label>~,
+                input_html =>
+qq~<input type="text" name="minlinksig" id="minlinksig" size="5" value="$minlinksig" />~,
+                name     => 'minlinksig',
+                validate => 'number',
+            },
+            {
+                description =>
+qq~<label for="minlinkweb">$admin_txt{'minlinkweb'}<br /><span class="small">$admin_txt{'minlinkweb_exp'}</span></label>~,
+                input_html =>
+qq~<input type="text" name="minlinkweb" id="minlinkweb" size="5" value="$minlinkweb" />~,
+                name     => 'minlinkweb',
+                validate => 'number',
+            },
+            {
+                description =>
+qq~<label for="spd_detention_time">$admin_txt{'92'}<br /><span class="small">$admin_txt{'93'}</span></label>~,
+                input_html =>
+qq~<input type="text" name="spd_detention_time" id="spd_detention_time" size="5" value="$spd_detention_time" />~,
+                name     => 'spd_detention_time',
+                validate => 'number',
+            },
+            {
+                description =>
+                  qq~<label for="timeout">$admin_txt{'408'}</label>~,
+                input_html =>
+qq~<input type="text" name="timeout" id="timeout" size="4" value="$timeout" />~,
+                name     => 'timeout',
+                validate => 'number',
+            },
+            {
+                description =>
+                  qq~<label for="honeypot">$admin_txt{'honeypot'}</label>~,
+                input_html =>
+qq~<input type="checkbox" name="honeypot" id="honeypot" value="1"${ischecked($honeypot)} />~,
+                name     => 'honeypot',
+                validate => 'boolean',
+            },
+            {
+                description =>
+                  qq~<label for="spamfruits">$admin_txt{'spamfruits'}</label>~,
+                input_html =>
+qq~<input type="checkbox" name="spamfruits" id="spamfruits" value="1"${ischecked($spamfruits)} />~,
+                name     => 'spamfruits',
+                validate => 'boolean',
+            },
+            { header => $settings_txt{'speedban'}, },
+            {
+                description =>
+                  qq~<label for="speedpostdetection">$admin_txt{'89'}</label>~,
+                input_html =>
+qq~<input type="checkbox" name="speedpostdetection" id="speedpostdetection" value="1" ${ischecked($speedpostdetection)}/>~,
+                name     => 'speedpostdetection',
+                validate => 'boolean',
+            },
+            {
+                description =>
+                  qq~<label for="min_post_speed">$admin_txt{'90'}</label>~,
+                input_html =>
+qq~<input type="text" name="min_post_speed" id="min_post_speed" size="5" value="$min_post_speed" />~,
+                name       => 'min_post_speed',
+                validate   => 'number',
+                depends_on => ['speedpostdetection'],
+            },
+        ],
+    },
+    {
+        name  => $tsc_txt{'2'},
+        id    => 'tsc',
+        items => [
+            {
+                description =>
+qq~<label for="spamrules"><b>$tsc_txt{'4'}</b><br /><span class="small">$tsc_txt{'3'}</span></label>~,
+                input_html =>
+qq~<textarea cols="60" rows="35" name="spamrules" id="spamrules" style="width: 95%">$spamlist</textarea>~,
+                two_rows => 1,
+                name     => 'spamrules',
+                validate => 'fulltext,null',
+            },
+        ],
+    },
+    {
+        name  => $domain_filter_txt{'2'},
+        id    => 'emailfilter',
+        items => [
+            {
+                description =>
+qq~<label for="adomains"><b>$domain_filter_txt{'4'}</b><br /><span class="small">$domain_filter_txt{'3'}</span></label>~,
+                input_html =>
+qq~<textarea cols="60" rows="35" name="adomains" id="adomains" style="width: 95%">$adomains</textarea>~,
+                two_rows => 1,
+                name     => 'adomains',
+                validate => 'fulltext,null',
+            },
+            {
+                description =>
+qq~<label for="bdomains"><b>$domain_filter_txt{'6'}</b><br /><span class="small">$domain_filter_txt{'7'}</span></label>~,
+                input_html =>
+qq~<textarea cols="60" rows="35" name="bdomains" id="bdomains" style="width: 95%">$bdomains</textarea>~,
+                two_rows => 1,
+                name     => 'bdomains',
+                validate => 'fulltext,null',
+            },
+        ],
+    },
 );
 
 # Routine to save them
 sub SaveSettings {
-	my %settings = @_;
+    my %settings = @_;
 
-	# TSC
-	$settings{'spamrules'} =~ s/\r(?=\n*)//g;
-	fopen(SPAM, ">$vardir/spamrules.txt");
-	print SPAM delete($settings{'spamrules'});
-	fclose(SPAM);
+    # TSC
+    $settings{'spamrules'} =~ s/\r(?=\n*)//gxsm;
+    no strict;
+    fopen( SPAM, ">$vardir/spamrules.txt" );
+    print {SPAM} delete $settings{'spamrules'} or croak 'cannot print SPAM';
+    fclose(SPAM);
+    use strict;
 
-	# email domain filter
-	my @domains = (delete $settings{'adomains'}, delete $settings{'bdomains'});
-	foreach (@domains){
-		s~\n~,~g;
-		s~\s+~~g;
-		s~(^,+|,+$)~~g;
-		s~,+~,~g;
-		s~\@~\\@~g;
-	}
-	fopen(FILE, ">$vardir/email_domain_filter.txt");
-	print FILE qq~\$adomains = "$domains[0]";\n~;
-	print FILE qq~\$bdomains = "$domains[1]";\n~;
-	print FILE qq~1;~;
-	fclose(FILE);
+    # email domain filter
+    my @domains =
+      ( delete $settings{'adomains'}, delete $settings{'bdomains'} );
+    foreach (@domains) {
+        s/\n/,/gxsm;
+        s/\s+//gxsm;
+        s/(^,+|,+$)//gxsm;
+        s/,+/,/gxsm;
+        s/\@/\\@/gxsm;
+    }
+    no strict;
+    fopen( FILE, ">$vardir/email_domain_filter.txt" );
+    print {FILE} qq~\$adomains = "$domains[0]";\n~ or croak 'cannot print FILE';
+    print {FILE} qq~\$bdomains = "$domains[1]";\n~ or croak 'cannot print FILE';
+    print {FILE} q~1;~                             or croak 'cannot print FILE';
+    fclose(FILE);
+    use strict;
 
-	# Settings.pl
-	&SaveSettingsTo('Settings.pl', %settings);
+    # Settings.pl
+    SaveSettingsTo( 'Settings.pl', %settings );
+    return;
 }
 
 1;
