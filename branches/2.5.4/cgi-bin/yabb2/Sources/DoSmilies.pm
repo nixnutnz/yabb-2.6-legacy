5739e96ec4793ccc3cc5d787d2ae89b781cf9a90
dandello2000 <dandello2000@users.sourceforge.net>
2013-03-08 05:33:36 +0000

    Css issues with Advanced Smilies window
    git-svn-id: https://svn.code.sf.net/p/yabb/svn@878 5281d325-37d3-4172-b7de-bd13e9c9f870

--- branches/2.5.4/cgi-bin/yabb2/Sources/DoSmilies.pm
+++ branches/2.5.4/cgi-bin/yabb2/Sources/DoSmilies.pm
@@ -14,7 +14,7 @@
 our $VERSION = '2.5.4';
 
 $dosmiliespmver = 'YaBB 2.5.4 $Revision$';
-if ( $action eq 'detailedversion' ) { return 1; }
+if ($action eq 'detailedversion') { return 1; }
 
 LoadLanguage('Main');
 
@@ -45,7 +45,7 @@ qq~<td><img src="$tmpurl" alt="$SmilieDescription[$i]" onclick="javascript:MoreS
         opendir DIR, "$htmldir/Smilies";
         @contents = readdir DIR;
         closedir DIR;
-        $smilieslist = {};
+        $smilieslist = q{};
         foreach my $line ( sort { uc $a cmp uc $b } @contents ) {
             ( $name, $extension ) = split /\./xsm, $line;
             if (   $extension =~ /gif/ism
@@ -80,28 +80,28 @@ td {border: #ccc solid thin; text-align:center; height:40px; width:60px;}
 <script type="text/javascript">
 <!--
 function AddText(text) {
-    if (window.opener && !window.opener.closed) {
-        if (opener.document.postmodify.message.createTextRange && opener.document.postmodify.message.caretPos) {
-            var caretPos = opener.document.postmodify.message.caretPos;
-            caretPos.text = caretPos.text.charAt(caretPos.text.length - 1) == ' ' ?
-            text + ' ' : text;
-        } else if (opener.document.postmodify.message.setSelectionRange) {
-            var selectionStart = opener.document.postmodify.message.selectionStart;
-            var selectionEnd = opener.document.postmodify.message.selectionEnd;
-            var replaceString = text + opener.document.postmodify.message.value.substring(selectionStart, selectionEnd);
-            opener.document.postmodify.message.value = opener.document.postmodify.message.value.substring(0, selectionStart) + replaceString + opener.document.postmodify.message.value.substring(selectionEnd);
-            opener.document.postmodify.message.setSelectionRange(selectionStart + text.length, selectionEnd + text.length);
-        } else {
-            opener.document.postmodify.message.value += text;
-        }
-    }
+	if (window.opener && !window.opener.closed) {
+		if (opener.document.postmodify.message.createTextRange && opener.document.postmodify.message.caretPos) {
+			var caretPos = opener.document.postmodify.message.caretPos;
+			caretPos.text = caretPos.text.charAt(caretPos.text.length - 1) == ' ' ?
+			text + ' ' : text;
+		} else if (opener.document.postmodify.message.setSelectionRange) {
+			var selectionStart = opener.document.postmodify.message.selectionStart;
+			var selectionEnd = opener.document.postmodify.message.selectionEnd;
+			var replaceString = text + opener.document.postmodify.message.value.substring(selectionStart, selectionEnd);
+			opener.document.postmodify.message.value = opener.document.postmodify.message.value.substring(0, selectionStart) + replaceString + opener.document.postmodify.message.value.substring(selectionEnd);
+			opener.document.postmodify.message.setSelectionRange(selectionStart + text.length, selectionEnd + text.length);
+		} else {
+			opener.document.postmodify.message.value += text;
+		}
+	}
 }
 
 moresmiliecode = new Array($more_smilie_array)
 
 function MoreSmilies(i) {
-    AddTxt=moresmiliecode[i];
-    AddText(AddTxt);
+	AddTxt=moresmiliecode[i];
+	AddText(AddTxt);
 }
 // -->
 </script>
@@ -112,11 +112,11 @@ function MoreSmilies(i) {
     <tr>~;
 
     if ( $showadded == 3 || ( $showadded == 2 && $detachblock == 1 ) ) {
-        $output .= qq~ $moresmilieslist ~;
-    }
+		$output .= qq~ $moresmilieslist ~;
+	}
 
-    $output .= qq~
-    $evenmoresmilies
+	$output .= qq~
+	$evenmoresmilies
 </tr></table>
 </body>
 </html>~;
@@ -128,35 +128,35 @@ function MoreSmilies(i) {
 sub SmilieIndex {
     print_output_header();
 
-    $i                 = 0;
-    $offset            = 0;
+	$i = 0;
+	$offset = 0;
     $smilieslist       = q{};
     $smilie_code_array = q{};
     if ( $showadded == 3 || ( $showadded == 2 && $detachblock == 1 ) ) {
-        while ( $SmilieURL[$i] ) {
-            if ( $i % 4 == 0 && $i != 0 ) {
+		while ($SmilieURL[$i]) {
+			if ($i % 4 == 0 && $i != 0) {
                 $smilieslist .= qq~      </tr><tr>\n~;
-                $offset++;
-            }
+				$offset++;
+			}
             if ( ( $i + $offset ) % 2 == 0 ) {
-                $smiliescolor = q~class="windowbg2"~;
+                $smiliescolor = q~windowbg2~;
             }
-            else { $smiliescolor = q~class="windowbg"~; }
+            else { $smiliescolor = q~windowbg~; }
             if ( $SmilieURL[$i] =~ /\//ixsm ) { $tmpurl = $SmilieURL[$i]; }
-            else { $tmpurl = qq~$defaultimagesdir/$SmilieURL[$i]~; }
+			else { $tmpurl = qq~$defaultimagesdir/$SmilieURL[$i]~; }
             $smilieslist .=
-qq~          <td class="center h_60px" $smiliescolor><img src="$tmpurl" alt="" onclick='javascript:MoreSmilies($i)' style='cursor:hand' /><br /><span style="font-size:xx-small; color:#$poptext">$SmilieDescription[$i]</span></td>\n~;
-            $smilie_url_array .= qq~"$tmpurl", ~;
-            $tmpcode = $SmilieCode[$i];
+qq~          <td class="center h_60px $smiliescolor"><img src="$tmpurl" alt="" onclick='javascript:MoreSmilies($i)' style='cursor:hand' /><br /><span style="font-size:xx-small; color:#$poptext">$SmilieDescription[$i]</span></td>\n~;
+			$smilie_url_array .= qq~"$tmpurl", ~;
+			$tmpcode = $SmilieCode[$i];
             $tmpcode =~ s/\&quot;/"+'"'+"/gxsm;    #';
             FromHTML($tmpcode);
             $tmpcode =~ s/&#36;/\$/gxsm;
             $tmpcode =~ s/&#64;/\@/gxsm;
-            $more_smilie_array .= qq~" $tmpcode", ~;
-            $i++;
-        }
-    }
-    if ( $showsmdir == 3 || ( $showsmdir == 2 && $detachblock == 1 ) ) {
+			$more_smilie_array .= qq~" $tmpcode", ~;
+			$i++;
+		}
+	}
+	if ($showsmdir == 3 || ($showsmdir == 2 && $detachblock == 1)) {
         opendir DIR, "$htmldir/Smilies";
         @contents = readdir DIR;
         closedir DIR;
@@ -168,41 +168,41 @@ qq~          <td class="center h_60px" $smiliescolor><img src="$tmpurl" alt="" o
                 || $extension =~ /png/ixsm )
             {
                 if ( $line !~ /banner/ixsm ) {
-                    if ( $i % 4 == 0 && $i != 0 ) {
+					if ($i % 4 == 0 && $i != 0) {
                         $smilieslist .= qq~      </tr><tr>\n~;
-                        $offset++;
-                    }
+						$offset++;
+					}
                     if ( ( $i + $offset ) % 2 == 0 ) {
-                        $smiliescolor = q~class="windowbg2"~;
+                        $smiliescolor = q~windowbg2~;
                     }
-                    else { $smiliescolor = q~class="windowbg"~; }
+                    else { $smiliescolor = q~windowbg~; }
                     $smilieslist .=
-qq~          <td class="center h_60px" $smiliescolor><img src="$yyhtml_root/Smilies/$line" alt="" onclick="javascript:MoreSmilies($i)" style="cursor:hand" /><br /><span style="font-size: xx-small; color:#$poptext">$line</span></td>\n~;
-                    $more_smilie_array .= qq~" [smiley=$line]", ~;
-                    $i++;
-                }
-            }
-        }
-    }
-    while ( $i % 4 != 0 ) {
+qq~          <td class="center h_60px $smiliescolor"><img src="$yyhtml_root/Smilies/$line" alt="" onclick="javascript:MoreSmilies($i)" style="cursor:hand" /><br /><span style="font-size: xx-small; color:#$poptext">$line</span></td>\n~;
+					$more_smilie_array .= qq~" [smiley=$line]", ~;
+					$i++;
+				}
+			}
+		}
+	}
+	while ($i % 4 != 0) {
         if ( ( $i + $offset ) % 2 == 0 ) {
-            $smiliescolor = q~class="windowbg2"~;
+            $smiliescolor = q~windowbg2~;
         }
-        else { $smiliescolor = q~class="windowbg"~; }
+        else { $smiliescolor = q~windowbg~; }
         $smilieslist .=
-          qq~          <td class="center h_60px" $smiliescolor>&nbsp;</td>\n~;
-        $i++;
-    }
+          qq~          <td class="center h_60px $smiliescolor">&nbsp;</td>\n~;
+		$i++;
+	}
     $smilie_code_array .= q~""~;
     $more_smilie_array .= q~""~;
-    if ( -e "$htmldir/Smilies/banner.gif" ) {
+	if (-e "$htmldir/Smilies/banner.gif") {
         $smiliesheader = qq~<tr>
     <td class="center" style="background-color:#$popback" colspan="4">
         <img src="$yyhtml_root/Smilies/banner.gif" alt="" />
     </td>
 </tr>~;
     }
-    else {
+	else {
         $smiliesheader =
 qq~<tr><td class="center" colspan="4"><b><span style="font-size:small">$smiltxt{'21'}</span></b></td></tr>~;
     }
@@ -217,27 +217,27 @@ qq~<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3
 <script type="text/javascript">
 <!--
 function AddText(text) {
-    if (window.opener && !window.opener.closed) {
-        if (opener.document.postmodify.message.createTextRange && opener.document.postmodify.message.caretPos) {
-            var caretPos = opener.document.postmodify.message.caretPos;
-            caretPos.text = caretPos.text.charAt(caretPos.text.length - 1) == ' ' ?
-            text + ' ' : text;
-        } else if (opener.document.postmodify.message.setSelectionRange) {
-            var selectionStart = opener.document.postmodify.message.selectionStart;
-            var selectionEnd = opener.document.postmodify.message.selectionEnd;
-            var replaceString = text + opener.document.postmodify.message.value.substring(selectionStart, selectionEnd);
-            opener.document.postmodify.message.value = opener.document.postmodify.message.value.substring(0, selectionStart) + replaceString + opener.document.postmodify.message.value.substring(selectionEnd);
-            opener.document.postmodify.message.setSelectionRange(selectionStart + text.length, selectionEnd + text.length);
-        } else {
-            opener.document.postmodify.message.value += text;
-        }
-    }
+	if (window.opener && !window.opener.closed) {
+		if (opener.document.postmodify.message.createTextRange && opener.document.postmodify.message.caretPos) {
+			var caretPos = opener.document.postmodify.message.caretPos;
+			caretPos.text = caretPos.text.charAt(caretPos.text.length - 1) == ' ' ?
+			text + ' ' : text;
+		} else if (opener.document.postmodify.message.setSelectionRange) {
+			var selectionStart = opener.document.postmodify.message.selectionStart;
+			var selectionEnd = opener.document.postmodify.message.selectionEnd;
+			var replaceString = text + opener.document.postmodify.message.value.substring(selectionStart, selectionEnd);
+			opener.document.postmodify.message.value = opener.document.postmodify.message.value.substring(0, selectionStart) + replaceString + opener.document.postmodify.message.value.substring(selectionEnd);
+			opener.document.postmodify.message.setSelectionRange(selectionStart + text.length, selectionEnd + text.length);
+		} else {
+			opener.document.postmodify.message.value += text;
+		}
+	}
 }
 
 moresmiliecode = new Array($more_smilie_array)
 function MoreSmilies(i) {
-    AddTxt=moresmiliecode[i];
-    AddText(AddTxt);
+	AddTxt=moresmiliecode[i];
+	AddText(AddTxt);
 }
 //-->
 </script>
