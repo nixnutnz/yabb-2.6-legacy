c2d0e2c67d6c5af9dcfe2bab5e681843761cf04e
dandello2000 <dandello2000@users.sourceforge.net>
2014-02-05 23:11:51 +0000

    issue with broadcast.message when missing
    git-svn-id: https://svn.code.sf.net/p/yabb/svn@1411 5281d325-37d3-4172-b7de-bd13e9c9f870

--- branches/2.5.4RC1/cgi-bin/yabb2/Convert2x.pl
+++ branches/2.5.4RC1/cgi-bin/yabb2/Convert2x.pl
@@ -620,6 +620,7 @@ sub MyUpdateUser {
     print {NMEMINFO} @meminfo or croak 'cannot print NBMEMINFO';
     fclose(NMEMINFO);
 
+	if ( -e "$convmemberdir/broadcast.messages" ) {
     fopen( BMEMDIR, "$convmemberdir/broadcast.messages" )
       || setup_fatal_error( "$maintext_23 $convmemberdir/broadcast.messages: ", 1 );
     my @bmessages = <BMEMDIR>;
@@ -629,6 +630,8 @@ sub MyUpdateUser {
       || setup_fatal_error( "$maintext_23 $convmemberdir/broadcast.messages: ", 1 );
     print {NBMEMDIR} @bmessages or croak 'cannot print NBMEMDIR';
     fclose(NBMEMDIR);
+	}
+
     return;
 }
 
@@ -987,7 +990,7 @@ qq~<link rel="stylesheet" href="$yyhtml_root/Templates/Forum/$usestyle.css" type
         if (
             !$yycopyin
             && (   $curline =~ m{<yabb\ copyright>}xsm
-                || $curline =~ m/{yabb\ copyright}/xsm )
+                || $curline =~ /{yabb\ copyright}/xsm )
           )
         {
             $yycopyin = 1;
@@ -1050,7 +1053,7 @@ qq~<link rel="stylesheet" href="$yyhtml_root/Templates/Forum/$usestyle.css" type
     }
     if ( $yycopyin == 0 ) {
         $output =
-q~<h1 style="text-align:center"><b>Sorry, the copyright tag &#123;yabb copyright&#125; must be in the template.<br />Please notify this forum&#39;s administrator that this site is using an ILLEGAL copy of YaBB!</b></h1>~;
+q~<h1 style="text-align:center"><b>Sorry, the copyright tag <yabb copyright> must be in the template.<br />Please notify this forum&#39;s administrator that this site is using an ILLEGAL copy of YaBB!</b></h1>~;
     }
     if ( fileno $GZIP ) {
         $OUTPUT_AUTOFLUSH = 1;
