c7003b86e9fe3a2f5e49cdc1faaa94cc3ba307b9
dandello2000 <dandello2000@users.sourceforge.net>
2014-02-21 03:33:15 +0000

    Renamed 2.5.4 to 2.6.0
    git-svn-id: https://svn.code.sf.net/p/yabb/svn@1416 5281d325-37d3-4172-b7de-bd13e9c9f870

---
+++ branches/2.6/cgi-bin/yabb2/Admin/Ban.pm
@@ -0,0 +1,552 @@
+###############################################################################
+# Ban.pm                                                                      #
+# $Date: 02.20.14 $                                                           #
+###############################################################################
+# YaBB: Yet another Bulletin Board                                            #
+# Open-Source Community Software for Webmasters                               #
+# Version:        YaBB 2.6.0                                                  #
+# Packaged:       February 20, 2014                                           #
+# Distributed by: http://www.yabbforum.com                                    #
+# =========================================================================== #
+# Copyright (c) 2000-2014 YaBB (www.yabbforum.com) - All Rights Reserved.     #
+# Software by:  The YaBB Development Team                                     #
+#               with assistance from the YaBB community.                      #
+###############################################################################
+use CGI::Carp qw(fatalsToBrowser);
+use CGI qw(:standard);
+use Time::Local 'timelocal';
+our $VERSION = '2.6.0';
+
+$banpmver = 'YaBB 2.6.0 $Revision$';
+
+sub ipban {
+    is_admin_or_gmod();
+
+    use Time::localtime;
+    fopen( BAN, "<$vardir/banlist.txt" ) || fatal_error( 'cannot_open', "$vardir/banlist.txt", 1 );
+    @banlist = <BAN>;
+    fclose(BAN);
+    my @timeban = qw( p d w m );
+    my @bandays = ( 365, 1, 7, 30, );
+    $today = time;
+
+    *time_ban = sub {
+        my $ban_user = $banned[3];
+        for my $i ( 0 .. 3 ) {
+            $tm   = localtime $banned[2];
+            $year = $tm->year + 1900;
+            $mon  = $tm->mon + 1;
+            $day  = $tm->mday;
+
+            if ( $banned[4] eq $timeban[$i] ) {
+                $tmb = $banned[2] + ( $bandays[$i] * 86_400 );
+            }
+        }
+
+        if ( $banned[4] eq 'p' ) {
+            $timeb =
+"$mon/$day/$year by ${$uid.$ban_user}{'realname'} ($ban_user) - Permanent";
+        }
+        elsif ( $banned[4] ne 'p' && $tmb < $today ) {
+            $timeb =
+"$mon/$day/$year by ${$uid.$ban_user}{'realname'} ($ban_user) - Expired";
+        }
+        else {
+            $tma   = localtime $tmb;
+            $yearb = $tma->year + 1900;
+            $monb  = $tma->mon + 1;
+            $dayb  = $tma->mday;
+            $timeb =
+qq~$mon/$day/$year by ${$uid.$ban_user}{'realname'} ($ban_user) - Expires on: $monb/$dayb/$yearb~;
+        }
+        return $timeb;
+    };
+
+    for my $i (@banlist) {
+        chomp $i;
+        my $ii = 0;
+        my $ee = 0;
+        my $uu = 0;
+        @banned = split /\|/xsm, $i;
+        if ( $banned[0] eq 'I' ) {
+            $ban_i    = $banned[1];
+            $timebana = time_ban();
+            $iban .= qq~<option value="$i"> $ban_i - $timebana</option>\n~;
+            $ii ++;
+        }
+        if ( $banned[0] eq 'E' ) {
+            $ban_e  = $banned[1];
+            $e_show = $banned[1];
+            $e_show =~ s/\\@/@/xsm;
+            $timebana = time_ban();
+            $eban .= qq~<option value="$i"> $e_show - $timebana</option>\n~;
+            $ee++;
+        }
+        if ( $banned[0] eq 'U' ) {
+            $ban_u = $banned[1];
+            $timebana = time_ban();
+            $uban .= qq~<option value="$i"> $ban_u - $timebana</option>\n~;
+            $uu++;
+        }
+    }
+    if ( $ii == 0 ) { $iban .= q~<option value=""> </option>~; }
+    if ( $ee == 0 ) { $eban .= q~<option value=""> </option>~; }
+    if ( $uu == 0 ) { $uban .= q~<option value=""> </option>~; }
+
+    $yymain .= qq~
+    <form action="$adminurl?action=ipban2" method="post">
+    <div class="bordercolor rightboxdiv">
+        <table class="border-space pad-cell" style="margin-bottom: .5em;">
+            <tr>
+                <td class="titlebg">
+                    $admin_img{'banimg'} <b>$admin_txt{'340'}</b>
+                </td>
+            </tr><tr>
+                <td class="catbg">
+                    <label for="iban"><span class="small">$admin_txt{'724a'}</span></label>
+                </td>
+            </tr><tr>
+                <td class="windowbg2">
+                    <select id="iban" name="iban" size="20" multiple="multiple" style="min-width:45%">
+                        $iban
+                    </select>
+                </td>
+            </tr>
+        </table>
+    </div>
+    <div class="bordercolor rightboxdiv">
+        <table class="border-space pad-cell" style="margin-bottom: .5em;">
+            <tr>
+                <th class="titlebg">$admin_img{'prefimg'} $admin_txt{'10'}</th>
+            </tr><tr>
+                <td class="catbg center">
+                    <input type="submit" value="$admin_txt{'10'}" class="button" />
+                </td>
+            </tr>
+        </table>
+    </div>
+    </form>
+    <form action="$adminurl?action=ipban2" method="post">
+    <div class="bordercolor borderstyle rightboxdiv">
+        <table class="border-space pad-cell" style="margin-bottom: .5em;">
+            <tr>
+                <td class="catbg">
+                    <label for="eban"><span class="small">$admin_txt{'725b'}</span></label>
+                </td>
+            </tr><tr>
+                <td class="windowbg2">
+                    <select id="eban" name="eban" size="20" multiple="multiple" style="min-width:45%">
+                        $eban
+                    </select>
+                </td>
+            </tr>
+        </table>
+    </div>
+    <div class="bordercolor rightboxdiv">
+    <table class="border-space pad-cell" style="margin-bottom: .5em;">
+        <tr>
+            <th class="titlebg">$admin_img{'prefimg'} $admin_txt{'10'}</th>
+        </tr><tr>
+            <td class="catbg center">
+                <input type="submit" value="$admin_txt{'10'}" class="button" />
+            </td>
+        </tr>
+    </table>
+    </div>
+    </form>
+    <form action="$adminurl?action=ipban2" method="post">
+    <div class="bordercolor borderstyle rightboxdiv">
+        <table class="border-space pad-cell" style="margin-bottom: .5em;">
+            <tr>
+                <td class="catbg">
+                    <label for="uban"><span class="small">$admin_txt{'725c'}</span></label>
+                </td>
+            </tr><tr>
+                <td class="windowbg2">
+                    <select id="uban" name="uban" size="20" multiple="multiple" style="min-width:45%">
+                        $uban
+                    </select>
+                </td>
+            </tr>
+        </table>
+    </div>
+    <div class="bordercolor rightboxdiv">
+    <table class="border-space pad-cell" style="margin-bottom: .5em;">
+        <tr>
+            <th class="titlebg">$admin_img{'prefimg'} $admin_txt{'10'}</th>
+        </tr><tr>
+            <td class="catbg center">
+                <input type="submit" value="$admin_txt{'10'}" class="button" />
+            </td>
+        </tr>
+    </table>
+    </div>
+    </form>
+        <form action="$adminurl?action=ipban_add" method="post">
+        <div class="bordercolor rightboxdiv">
+        <table class="border-space pad-cell" style="margin-bottom: .5em;">
+            <col span="2" style="width: 50%" />
+            <tr>
+                <td class="titlebg">
+                    $admin_img{'banimg'} <b>$admin_txt{'340a'}</b>
+                </td>
+                <td class="titlebg">
+                    <b>$admin_txt{'340b'}</b>
+                </td>
+            </tr><tr>
+                <td class="catbg" style="background-repeat:repeat-x">
+                    <span class="small">$admin_txt{'724'}<br />$admin_txt{'725'}<br />$admin_txt{'725a'}</span>
+                </td>
+                <td class="windowbg2 vtop" rowspan="2">
+                    <div style="height:20em; overflow:auto">
+                    <ul>~;
+    $yymain .= banlog();
+    $yymain .= qq~            </ul>
+                    </div>
+                </td>
+            </tr><tr>
+                <td class="windowbg2">
+               $admin_txt{'340c'}<br /><input type='radio' name='type' value='U' />$admin_txt{'340d'}
+               <br /><input type='radio' name='type' value='I' checked="checked" />$admin_txt{'340e'}
+               <br /><input type='radio' name='type' value='E' />$admin_txt{'307'}
+               <br /><textarea rows="10" cols="100" name="banned" style="width:90%"></textarea>
+                <input type="hidden" name="unban" value="1" />
+                </td>
+            </tr>
+        </table>
+    </div>
+    <div class="bordercolor rightboxdiv">
+    <table class="border-space pad-cell" style="margin-bottom: .5em;">
+        <tr>
+            <th class="titlebg">$admin_img{'prefimg'} $admin_txt{'10'}</th>
+        </tr><tr>
+            <td class="catbg center">
+                <input type="submit" value="$admin_txt{'10'}" class="button" />
+            </td>
+        </tr>
+    </table>
+    </div>
+    </form>
+    <form action="$adminurl?action=ban_clean" method="post">
+        <div class="bordercolor rightboxdiv">
+        <table class="border-space pad-cell">
+            <tr>
+                <td class="titlebg">
+                    $admin_img{'banimg'} <b>$admin_txt{'725d'}</b>
+                </td>
+            </tr><tr>
+                <td class="catbg center">
+                    <span class="small">$admin_txt{'725g'}</span><br />
+                    <input type="submit" value="$admin_txt{'725e'}" class="button" />
+                </td>
+            </tr>
+        </table>
+        </div>
+    </form>
+~;
+
+    $yytitle     = "$admin_txt{'340'}";
+    $action_area = 'ipban';
+    AdminTemplate();
+    return;
+}
+
+sub ipban2 {
+    is_admin_or_gmod_or_fmod();
+    my $ban_u = $FORM{'uban'};
+    my $ban_e = $FORM{'eban'};
+    my $ban_i = $FORM{'iban'};
+
+    fopen( BAN, "<$vardir/banlist.txt" ) || fatal_error( 'cannot_open', "$vardir/banlist.txt", 1 );
+    my @myban = <BAN>;
+    chomp @myban;
+    fclose(BAN);
+
+    fopen( BAN2, ">$vardir/banlist.txt" ) || fatal_error( 'cannot_open', "$vardir/banlist.txt", 1 );
+    my @banned_u = split /\, /sm, $ban_u;
+    chomp @banned_u;
+    my @banned_e = split /\, /sm, $ban_e;
+    chomp @banned_e;
+    my @banned_i = split /\, /sm, $ban_i;
+    chomp @banned_i;
+    my %seen   = ();
+    my @allban = ();
+
+    *banning = sub {
+        my @in = @_;
+        foreach my $item (@in) { $seen{$item} = 1 }
+
+        foreach my $item (@myban) {
+            if ( !$seen{$item} ) {
+                push @allban, $item;
+            }
+        }
+    };
+
+    if ($ban_i) {
+        banning(@banned_i);
+    }
+    if ($ban_u) {
+        banning(@banned_u);
+    }
+    if ($ban_e) {
+        banning(@banned_e);
+    }
+
+    foreach my $j (@allban) {
+        print {BAN2} qq~$j\n~ or croak "$croak{'print'} UNBAN";
+    }
+    fclose(BAN2);
+
+    $yySetLocation = qq~$adminurl?action=ipban~;
+    redirectexit();
+    return;
+}
+
+sub ipban_add {
+    is_admin_or_gmod();
+    my $ban_in = $FORM{'banned'};
+    my $type   = $FORM{'type'};
+
+    my @banin = split /\n/xsm, $ban_in;
+
+    fopen( BAN, "<$vardir/banlist.txt" ) || fatal_error( 'cannot_open', "$vardir/banlist.txt", 1 );
+    my @myban = <BAN>;
+    chomp @myban;
+    fclose(BAN);
+    $time = time;
+    my $ihave = 0;
+    foreach my $j (@banin) {
+        $j =~ tr/\r//d;
+        $j =~ s/\A[\s\n]+| |[\s\n]+\Z//gsm;
+        $j =~ s/\n\s*\n/\n/gsm;
+        $j =~ s/@/\\@/xsm;
+        foreach my $i (@myban) {
+            @banned = split /\|/xsm, $i;
+            if ( $banned[1] eq $j ) {
+                $ihave = 1;
+            }
+        }
+
+        fopen( BAN2, ">>$vardir/banlist.txt" ) || fatal_error( 'cannot_open', "$vardir/banlist.txt", 1 );
+        if ( $j && $ihave == 0 && $j ne '127.0.0.1' ) {
+            print {BAN2}
+              qq~$type|$j|$time|${$uid.$username}{'realname'} ($username)|p|\n~
+              or croak "$croak{'print'} BAN2";
+        }
+        else { print {BAN2} q~~ or croak "$croak{'print'} BAN2"; }
+        fclose(BAN2);
+    }
+    $yySetLocation = qq~$adminurl?action=ipban~;
+    redirectexit();
+    return;
+}
+
+sub ipban_update {
+
+    # This is for quick updating for banning + unbanning
+    if ( $iamadmin || $iamgmod || $iamfmod ) {
+    my $ban       = $INFO{'ban'};
+    my $lev       = $INFO{'lev'};
+    my $ban_email = $INFO{'ban_email'};
+    my $ban_mem   = $INFO{'ban_memname'};
+    my $unban     = $INFO{'unban'};
+    my $user      = $INFO{'username'};
+    $ban_mem = $do_scramble_id ? decloak($ban_mem) : $ban_mem;
+    $ban_email =~ s/@/\\@/xsm;
+
+    my $time = time;
+    $ihave = 0;
+    $ehave = 0;
+    $uhave = 0;
+    fopen( BAN, "<$vardir/banlist.txt" ) || fatal_error( 'cannot_open', "$vardir/banlist.txt", 1 );
+    my @myban = <BAN>;
+    chomp @myban;
+    fclose(BAN);
+    if ( $unban != 1 ) {
+
+        foreach my $i (@myban) {
+            @banned = split /\|/xsm, $i;
+            if ($ban) {
+                if ( $banned[1] eq $ban ) {
+                    $ihave = 1;
+                }
+            }
+            elsif ($ban_email) {
+                if ( $banned[1] eq $ban_email ) {
+                    $ehave = 1;
+                }
+            }
+            elsif ($ban_mem) {
+                if ( $banned[1] eq $ban_mem ) {
+                    $uhave = 1;
+                }
+            }
+        }
+
+        fopen( BAN2, ">>$vardir/banlist.txt" ) || fatal_error( 'cannot_open', "$vardir/banlist.txt", 1 );
+        if ( $ban && $ihave == 0 && $ban ne '127.0.0.1' ) {
+            print {BAN2}
+              qq~I|$ban|$time|${$uid.$username}{'realname'} ($username)|$lev|\n~
+              or croak "$croak{'print'} BAN2";
+        }
+        if ( $ban_email && $ehave == 0 ) {
+            print {BAN2}
+qq~E|$ban_email|$time|${$uid.$username}{'realname'} ($username)|$lev|\n~
+              or croak "$croak{'print'} BAN2";
+        }
+        if ( $ban_mem && $uhave == 0 ) {
+            print {BAN2}
+qq~U|$ban_mem|$time|${$uid.$username}{'realname'} ($username)|$lev|\n~
+              or croak "$croak{'print'} BAN2";
+        }
+        fclose(BAN2);
+    }
+    elsif ( $unban == 1 ) {
+        fopen( BAN2, ">$vardir/banlist.txt" ) || fatal_error( 'cannot_open', "$vardir/banlist.txt", 1 );
+        foreach my $i (@myban) {
+            @banned = split /\|/xsm, $i;
+            if (   $ban eq $banned[1]
+                || $ban_email eq $banned[1]
+                || $ban_mem   eq $banned[1] )
+            {
+                $un_ban = q~~;
+            }
+            else {
+                $un_ban =
+                  qq~$banned[0]|$banned[1]|$banned[2]|$banned[3]|$banned[4]|\n~;
+            }
+            print {BAN2} $un_ban or "$croak{'print'} UNBAN";
+        }
+        fclose(BAN2);
+    }
+    $yySetLocation = qq~$scripturl?action=viewprofile;username=$user~;
+    redirectexit();
+    }
+    return;
+}
+
+sub ban_clean {
+    is_admin_or_gmod();
+
+    my @timeban = qw( p d w m );
+    my @bandays = ( 365, 1, 7, 30, );
+    my $time    = time;
+    fopen( BAN, "<$vardir/banlist.txt" ) || fatal_error( 'cannot_open', "$vardir/banlist.txt", 1 );
+    my @myban = <BAN>;
+    chomp @myban;
+    fclose(BAN);
+    fopen( BAN2, ">$vardir/banlist.txt" ) || fatal_error( 'cannot_open', "$vardir/banlist.txt", 1 );
+
+    *time_ban = sub {
+        for my $i ( 0 .. 3 ) {
+            if ( $banned[4] eq $timeban[$i] ) {
+                $tmb = $banned[2] + ( $bandays[$i] * 86_400 );
+            }
+        }
+    };
+    for my $j (@myban) {
+        @banned = split /\|/xsm, $j;
+        if ( $banned[4] eq 'p' ) {
+            print {BAN2}
+              qq~$banned[0]|$banned[1]|$banned[2]|$banned[3]|$banned[4]|\n~
+              or croak "$croak{'print'} BAN2";
+        }
+        else {
+            time_ban();
+            if ( $time > $tmb ) {
+                print {BAN2} q{} or croak "$croak{'print'} BAN2";
+            }
+            else {
+                print {BAN2}
+                  qq~$banned[0]|$banned[1]|$banned[2]|$banned[3]|$banned[4]|\n~
+                  or croak "$croak{'print'} BAN2";
+            }
+        }
+    }
+    fclose(BAN2);
+    $yySetLocation = qq~$adminurl?action=ipban~;
+    redirectexit();
+    return;
+}
+
+sub banlog {
+    fopen( BANLOG, "<$vardir/ban_log.txt" ) || fatal_error( 'cannot_open', "$vardir/ban_log.txt", 1 );
+    my @mybanlog = <BANLOG>;
+    chomp @mybanlog;
+    fclose(BANLOG);
+    use Time::localtime;
+    for my $ban (@mybanlog) {
+        @banned = split /\|/xsm, $ban;
+        $tm     = localtime $banned[0];
+        $year   = $tm->year + 1900;
+        $mon    = $tm->mon + 1;
+        $day    = $tm->mday;
+        if ( $use_guardian && $use_htaccess ) {
+            @banned_ip = ();
+            if ( $banned[1] =~ m/\(/sm ) {
+                @banned_ip = split /\(/xsm,$banned[1];
+                $banned_ip[1] =~ s/\)//xsm;
+                if ($banned_ip[0]) {
+                    $banned_ip[0] = qq~ ( $banned_ip[0] )~;
+                }
+                else {$banned_ip[0] = q~~;}
+            }
+            else {$banned_ip[1] = $banned[1];}
+            $banlog .=  qq~<li>$banned_ip[1]$banned_ip[0] - on $mon/$day/$year (<a href="$adminurl?action=guardian_block;ip=$banned_ip[1];return=ipban" onclick="return confirm('$admin_txt{'ipblock_confirm'}$banned_ip[1]');">$admin_txt{'ipblock'}</a>)</li>\n~;
+        }
+        else {
+        $banlog .=  qq~<li>$banned[1] on $mon/$day/$year</li>\n~;
+        }
+    }
+    return $banlog;
+}
+
+sub ipban_err {
+    is_admin_or_gmod();
+    my $ip_ban  = $INFO{'ban'};
+    my $lev     = $INFO{'lev'};
+    my @timeban = qw( p d w m );
+    my @bandays = ( 36_500, 1, 7, 30, );
+    my $tmb     = 0;
+
+    my $time  = time;
+    my $ihave = 0;
+    $ban =~ tr/\r//d;
+    $ban =~ s/\A[\s\n]+| |[\s\n]+\Z//gsm;
+    $ban =~ s/\n\s*\n/\n/gsm;
+    fopen( BAN, "<$vardir/banlist.txt" ) || fatal_error( 'cannot_open', "$vardir/banlist.txt", 1 );
+    my @myban = <BAN>;
+    chomp @myban;
+    fclose(BAN);
+
+    *time_ban = sub {
+        for my $i ( 0 .. 3 ) {
+            if ( $banned[4] eq $timeban[$i] ) {
+                $tmb = $banned[2] + ( $bandays[$i] * 84_600 );
+            }
+        }
+        return $tmb;
+    };
+    foreach my $i (@myban) {
+        @banned = split /\|/xsm, $i;
+        if ( $banned[0] eq 'I' && $banned[1] eq $ip_ban ) {
+            $tmb = time_ban();
+            if ( ( $banned[4] ne 'p' && $tmb > $today ) || $banned[4] eq 'p' ) {
+                $ihave = 1;
+            }
+        }
+    }
+
+    fopen( BAN2, ">>$vardir/banlist.txt" ) || fatal_error( 'cannot_open', "$vardir/banlist.txt", 1 );
+    if ( $ip_ban && $ihave == 0 && $ip_ban ne '127.0.0.1' ) {
+        print {BAN2} qq~I|$ip_ban|$time|${$uid.$username}{'realname'}|$lev|\n~
+          or croak "$croak{'print'} BAN2";
+    }
+    fclose(BAN2);
+
+    $yySetLocation = qq~$adminurl?action=$INFO{'return'}~;
+    redirectexit();
+    return;
+}
+1;
---
+++ branches/2.6/cgi-bin/yabb2/Admin/ModuleChecker.pm
@@ -0,0 +1,163 @@
+###############################################################################
+# ModuleChecker.pm                                                            #
+# $Date: 02.20.14 $                                                           #
+###############################################################################
+# YaBB: Yet another Bulletin Board                                            #
+# Open-Source Community Software for Webmasters                               #
+# Version:        YaBB 2.6.0                                                  #
+# Packaged:       February 20, 2014                                           #
+# Distributed by: http://www.yabbforum.com                                    #
+# =========================================================================== #
+# Copyright (c) 2000-2014 YaBB (www.yabbforum.com) - All Rights Reserved.     #
+# Software by:  The YaBB Development Team                                     #
+#               with assistance from the YaBB community.                      #
+###############################################################################
+use strict;
+use CGI::Carp qw(fatalsToBrowser);
+our $VERSION = '2.6.0';
+
+our $modulecheckerpmver = 'YaBB 2.6.0 $Revision$';
+my ( $dont_continue_setup );
+our ( $action, $yymain, %modulecheck );
+if ( $action eq 'detailedversion' ) { return 1; }
+
+my $script_root = $ENV{'SCRIPT_FILENAME'};
+if( ! $script_root ) {
+    $script_root = $ENV{'PATH_TRANSLATED'};
+}
+
+my ( $checker_output, $i );
+
+foreach my $module (
+    qw(Digest::MD5 Time::HiRes Time::Local File::Find CGI Net::SMTP Net::SMTP::TLS Compress::Zlib Compress::Bzip2 Archive::Tar Archive::Zip MIME::Lite LWP::UserAgent HTTP::Request::Common Crypt::SSLeay IO::Socket::INET Digest::HMAC_MD5 Carp bytes integer English)
+  )
+{
+    eval "require $module";
+
+    if ($@) {
+        if ( $module eq 'Digest::MD5' ) { $dont_continue_setup = 1; }
+        $i = $modulecheck{'8'};
+        my $e = $@;
+
+        # IE does display the @INC path it in one line  :-(
+        # If you use IE and don't like what you see, remove the
+        # comment (#) in next line.
+        # $e =~ s/\//\\/g;
+        $checker_output .= qq~<tr>
+                    <td class="windowbg2"><span class="important">$module</span></td>
+                    <td class="windowbg2">
+                        $modulecheck{'5'}<br />
+                        <br />$e
+                    </td>
+                    <td class="windowbg2">$modulecheck{"$module"}</td>
+                </tr>~;
+    }
+    else {
+        $checker_output .= qq~<tr>
+                    <td class="windowbg2"><span class="good">$module</span></td>
+                    <td class="windowbg2" colspan="2">$modulecheck{'6'}</td>
+                </tr>~;
+    }
+}
+
+if ( $script_root !~ /ModuleChecker\.\w+$/xsm ) {
+    $yymain .= qq~
+        <div class="bordercolor rightboxdiv" style="float: left; margin-top:.5em">
+            <table class="border-space pad-cell">
+                <tr>
+                    <td class="titlebg" colspan="3"><b>$modulecheck{'1'}</b></td>
+                </tr><tr>
+                    <td class="catbg" colspan="3">
+                        <span class="small">$modulecheck{'2'}</span>
+                    </td>
+                </tr>~ . (
+        $i
+        ? qq~<tr>
+                    <td class="windowbg2">
+                        <span class="important"><b>$modulecheck{'7'}</b></span>
+                    </td>
+                    <td class="windowbg2" colspan="2">$i</td>
+                </tr>~
+        : q{}
+      )
+      . qq~<tr>
+                    <td class="catbg center"><b>$modulecheck{'3'}</b></td>
+                    <td class="catbg center" colspan="2"><b>$modulecheck{'4'}</b></td>
+                </tr>
+            $checker_output
+            </table>
+        </div>~;
+
+}
+else {
+    my (%params);
+    print qq~Content-Type: text/html$params{'-charset'}\r\n
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<style type="text/css">
+table { width: 80%;
+margin:40px auto;
+border-collapse:separate;
+border-spacing: 1px;
+background-color: #A7B8CC;
+}
+td {padding:4px;}
+.catbg {
+    background-color: #FEFEFE;
+    color: #000000;
+    font-size: 13px;
+    text-align:center;
+}
+.important {
+color: #f00;
+}
+.titlebg {
+    background-color: #D2DBE6;
+    color: #475F79;
+    font-size: 13px;
+}
+.windowbg2 {
+    background-color: #FEFEFE;
+    color: #000000;
+    font-family: Verdana, sans-serif;
+    font-size: 11px;
+}
+</style>
+<title>YaBB 2.5.4 Module Checker</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+</head>
+<body>
+<table>
+    <tr>
+        <td class="titlebg" colspan="3">
+            <b>$modulecheck{'1'}</b><br />
+            $modulecheck{'2'}
+        </td>
+    </tr>~ . (
+        $i
+        ? qq~<tr>
+        <td class="windowbg2">
+            <span class="important"><b>$modulecheck{'7'}</b></span>
+        </td>
+        <td class="windowbg2" colspan="2">
+            $i
+        </td>
+    </tr>~
+        : q{}
+      )
+      . qq~<tr>
+        <td class="catbg">
+            <b>$modulecheck{'3'}</b>
+        </td>
+        <td class="catbg" colspan="2">
+            <b>$modulecheck{'4'}</b>
+        </td>
+    </tr>
+    $checker_output
+</table>
+</body>
+</html>~ or croak 'cannot print ModuleChecker';
+}
+
+1;
