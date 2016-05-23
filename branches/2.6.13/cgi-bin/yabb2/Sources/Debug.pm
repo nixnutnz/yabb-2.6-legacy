ff6da1717e289ffc98597c4656dab3d3ae787d4f
dandello2000 <dandello2000@users.sourceforge.net>
2016-05-23 15:39:19 +0000

    2.6.13 BASE - not working or updated copy.
    git-svn-id: https://svn.code.sf.net/p/yabb/svn@1718 5281d325-37d3-4172-b7de-bd13e9c9f870

---
+++ branches/2.6.13/cgi-bin/yabb2/Sources/Debug.pm
@@ -0,0 +1,54 @@
+###############################################################################
+# Debug.pm                                                                    #
+# $Date: 06.01.16 $                                                           #
+###############################################################################
+# YaBB: Yet another Bulletin Board                                            #
+# Open-Source Community Software for Webmasters                               #
+# Version:        YaBB 2.6.13                                                 #
+# Packaged:       June 1, 2016                                                #
+# Distributed by: http://www.yabbforum.com                                    #
+# =========================================================================== #
+# Copyright (c) 2000-2016 YaBB (www.yabbforum.com) - All Rights Reserved.     #
+# Software by:  The YaBB Development Team                                     #
+#               with assistance from the YaBB community.                      #
+###############################################################################
+our $VERSION = '2.6.13';
+
+$debugpmver = 'YaBB 2.6.13 $Revision: 1651 $';
+
+sub Debug {
+    if ( $debug == 1 || ( $debug == 2 && ( $iamadmin || $iamgmod ) ) || $debug == 3 ) {
+        $yyfileactions =
+"$debug_txt{'opened'} $file_open $debug_txt{'closed'} $file_close $debug_txt{'equal'}";
+
+        my $yytimeclock;
+        my $time_running = time - $START_TIME;
+        if ( $time_running == int $time_running ) {
+            $yytimeclock =
+              "$debug_txt{'nohires'} Time::Hires $debug_txt{'nomodule'}<br />";
+        }
+        else {
+            $time_running = sprintf '%.4f', $time_running;
+        }
+        $yytimeclock .=
+          "$debug_txt{'pagespeed'} $time_running $debug_txt{'loaded'}.";
+
+        ToHTML($openfiles);
+        $openfiles =~ s/\n/<br \/>/gxsm;
+
+        $yydebug =
+qq~<br /><div class="small debug"><span class="under">$debug_txt{'debugging'}<span><br /><br /><span class="under">$debug_txt{'benchmarking'}:</span><br />$yytimeclock<br /><br /><span class="under">$debug_txt{'ipaddress'}:</span><br />$user_ip<br /><br /><span class="under">$debug_txt{'browser'}:</span><br />$ENV{'HTTP_USER_AGENT'}<br />$getpairs<br /><span class="under">$debug_txt{'trace'}:</span>$yytrace<br /><br /><span class="under">$debug_txt{'check'}:</span><br />$yyfileactions<br /><br /><span class="under">$debug_txt{'filehandles'}:</span><br />$debug_txt{'filehandleslegend'}<br /><br />$openfiles<br /><span class="under">$debug_txt{'filesloaded'}:<span class="tt">require</span></span>~;
+
+        foreach ( sort keys %INC ) { $yydebug .= qq~<br />$_ => $INC{$_}~; }
+
+        $yydebug .= q~<br /><br /><br />
+    </div>~;
+        if ( $debug == 3 ) {
+            $yydebug =
+              qq~<br /><div class="small center debug">$yytimeclock</div>~;
+        }
+    }
+    return $yydebug;
+}
+
+1;
---
+++ branches/2.6.13/cgi-bin/yabb2/Sources/SpellChecker.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl --
+
+###############################################################################
+# SpellChecker.pl                                                             #
+# $Date: 06.01.16 $                                                           #
+###############################################################################
+# YaBB: Yet another Bulletin Board                                            #
+# Open-Source Community Software for Webmasters                               #
+# Version:        YaBB 2.6.13                                                 #
+# Packaged:       June 1, 2016                                                #
+# Distributed by: http://www.yabbforum.com                                    #
+# =========================================================================== #
+# Copyright (c) 2000-2016 YaBB (www.yabbforum.com) - All Rights Reserved.     #
+# Software by:  The YaBB Development Team                                     #
+#               with assistance from the YaBB community.                      #
+###############################################################################
+use CGI::Carp qw(fatalsToBrowser);
+our $VERSION = '2.6.13';
+
+$spellcheckerplver = 'YaBB 2.6.13 $Revision: 1651 $';
+if ( $action eq 'detailedversion' ) { return 1; }
+
+use LWP::UserAgent;
+use HTTP::Request::Common;
+
+$ua = LWP::UserAgent->new( agent => 'GoogieSpell Client' );
+$reqXML = q{};
+
+read STDIN, $reqXML, $ENV{'CONTENT_LENGTH'};
+
+$url = "http://orangoo.com/newnox?lang=$ENV{'QUERY_STRING'}";
+$res =
+  $ua->request(POST $url, Content_Type => 'text/xml', Content => $reqXML);
+
+croak "$res->{_content}" if $res->{_content} =~ /LWP.+https.+Crypt::SSLeay/sm;
+
+print "Content-Type: text/xml\n\n" or croak "$croak{'print'} content-type";
+print $res->{_content} or croak "$croak{'print'} speller";
+
+1;
