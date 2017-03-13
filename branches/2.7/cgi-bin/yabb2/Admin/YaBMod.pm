45d711dc13b6fdfc95fee5f9637c71824183ffee
dandello2000 <dandello2000@users.sourceforge.net>
2017-03-13 05:19:28 +0000

    typos
    git-svn-id: https://svn.code.sf.net/p/yabb/svn@1825 5281d325-37d3-4172-b7de-bd13e9c9f870

--- branches/2.7/cgi-bin/yabb2/Admin/YaBMod.pm
+++ branches/2.7/cgi-bin/yabb2/Admin/YaBMod.pm
@@ -1556,7 +1556,7 @@ sub yabm_deletemod {
    <div class="bordercolor rightboxdiv">
    <table class="border-space pad-cell" style="margin-bottom: .5em;">
      <tr>
-        <td class="titlebg"> <img src="$admin_images/boardmod_icon.png" alt="$yabmtxt{'20'}" /> <b>$yabmtxt{'20'}</b></td>
+        <td class="titlebg"> <img src="$admin_images/boardmod_icon.png" alt="$mod_list{'20'}" /> <b>$mod_list{'20'}</b></td>
      </tr>
      <tr>
     <td class="windowbg2">
