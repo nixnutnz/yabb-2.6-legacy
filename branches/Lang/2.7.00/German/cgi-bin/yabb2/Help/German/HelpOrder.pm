1d9704474c6d75c48dc7d70d2d995c721bc74fb2
dandello2000 <dandello2000@users.sourceforge.net>
2017-09-21 15:05:47 +0000

    Language Packs no longer added using mod - Setup mutli-language support
    git-svn-id: https://svn.code.sf.net/p/yabb/svn@1901 5281d325-37d3-4172-b7de-bd13e9c9f870

--- branches/Lang/2.7.00/German/cgi-bin/yabb2/Help/German/HelpOrder.pm
+++
@@ -1,4 +0,0 @@
-@admin = qw(001 admin01_cats admin02_boards admin03_members admin04_security admin05_templates admin06_advanced admin07_misc admin08_changes admin09_index );
-@gmod = qw(001 gmod01_threads gmod02_posts gmod03_ban gmod04_security);
-@moderator = qw(001 moderator01_threads moderator02_posts moderator03_ban);
-@user = qw(001 user00_agreement user01_begin user02_profile user03_posting user04_im user05_gloss);
