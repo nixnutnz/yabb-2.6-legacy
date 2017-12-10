83ae28f8d16c04ef0dd5a9a4ed895bf659343110
dandello2000 <dandello2000@users.sourceforge.net>
2017-12-10 16:47:20 +0000

    typos - update version
    git-svn-id: https://svn.code.sf.net/p/yabb/svn@1941 5281d325-37d3-4172-b7de-bd13e9c9f870

--- branches/2.7/cgi-bin/yabb2/Admin/DetailedVersion.pm
+++ branches/2.7/cgi-bin/yabb2/Admin/DetailedVersion.pm
@@ -24,7 +24,7 @@ use File::stat;
 use Digest::MD5;
 our $VERSION = '2.7.00';
 
-our $detailedversionpmver  = 'YaBB 2.7.00 $Revision: 1783 $';
+our $detailedversionpmver  = 'YaBB 2.7.00 $Revision: 1928 $';
 our @detailedversionpmmods = ();
 our $detailedversionpmmods = 0;
 if (@detailedversionpmmods) {
