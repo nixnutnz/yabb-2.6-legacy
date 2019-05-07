db4116bae188d192626d57d0d5dc5fde34c6d2f2
dandello2000 <dandello2000@users.sourceforge.net>
2019-05-07 12:10:07 +0000

    check for Crypt::SSLeay removed - redacted from YaBB 2.7
    git-svn-id: https://svn.code.sf.net/p/yabb/svn@2060 5281d325-37d3-4172-b7de-bd13e9c9f870

--- branches/2.7/cgi-bin/yabb2/Setup.pl
+++ branches/2.7/cgi-bin/yabb2/Setup.pl
@@ -2135,7 +2135,7 @@ sub modules {
     ($mylang) = @_;
     getlang($mylang);
     my @modules =
-      qw(Digest::MD5 Time::HiRes Time::Local DateTime DateTime::TimeZone File::Find CGI Net::SMTP Net::SMTPS Net::DNS Mail::CheckUser Compress::Zlib IO::Compress::Bzip2 Archive::Tar Archive::Zip MIME::Lite LWP::UserAgent HTTP::Request::Common Crypt::SSLeay IO::Socket::INET Digest::HMAC_MD5 Carp bytes integer English URI::Escape Module::Load );
+      qw(Digest::MD5 Time::HiRes Time::Local DateTime DateTime::TimeZone Locale::Country File::Find CGI Net::SMTP Net::SMTPS Net::DNS Mail::CheckUser Compress::Zlib IO::Compress::Bzip2 Archive::Tar Archive::Zip MIME::Lite LWP::UserAgent HTTP::Request::Common IO::Socket::INET Digest::HMAC_MD5 Carp bytes integer English URI::Escape Module::Load );
 
     @modules = sort @modules;
     my $checker_output = q{};
